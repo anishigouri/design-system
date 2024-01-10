d9e6df621b36b00931ba9aff3db81128fb5ea334
Allan Nishigouri <allanfurlani@gmail.com>
2024-01-10 09:58:51 -0300

    Criação dos tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@koin/react-webview'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
